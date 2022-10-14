1874aedd4b9caca5019c7f11236c65512fd855a9
lsx <191120844@qq.com>
2022-10-14 17:09:46 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ./conf/mysql/default.my.cnf  ./conf/mysql/my.cnf
+cp ./conf/php/default.php.ini  ./conf/php/php.ini
+
+cp ./conf/nginx/default.nginx.conf  ./conf/nginx/nginx.conf
+cp ./conf/nginx/conf.d/default.conf  ./conf/nginx/conf.d/localhost.conf
